38a11c14e7e7cb348c7e6154dc404e6b392c82ca
Luxary147 <95914993+Luxary147@users.noreply.github.com>
2022-04-22 21:29:31 +0200

    Añadido Login.php
    Se ha creado un archivo .php , el cual contiene un formulario , que se usara como login de nuestra aplicación .
    
    Dicho archivo actualmente tiene un formulario ,  como campos a introducir 
    -email
    -contraseña ( como tipo password )
    y un botón Login que es un submit
    
    Además  tiene un enlace , que nos redirigirá a un futuro archivo de register.php

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div id="formulario">
+
+        <h1> Login </h1>
+        <form action="login.php" method="post" id="formulario">
+            <input name="f_email" type="email" placeholder="e-mail" id ="f_email"><br>
+            <input name="f_password" type="password" placeholder="Contraseña" id ="f_password"><br>
+            <input type="submit" value="Login">
+        </form>
+
+        <a href="register.php"> No tienes cuenta, crea una</a>
+    </div>
+    
+</body>
+</html>

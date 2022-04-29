b12906eb22c97f1c4350ea52b065876a1f0ec276
Luxary147 <95914993+Luxary147@users.noreply.github.com>
2022-04-29 22:54:07 +0200

    Añadido el archivo do_login.php
    Este archivo contiene la lógica , para recuperar los datos del formulario de login .
    
    En el comprobamos si el correo introducido existe y se es así comparamos las contraseñas , y se estas son idénticas inicia una sesión .
    
    Lo voy a probar aun

---
+++ php/do_login.php
@@ -0,0 +1,29 @@
+<?php
+  $db = mysqli_connect('localhost', 'root', '1234', 'Ackernm') or die('Fail');
+
+  $email_posted = $_POST['l_email'];
+  $password_posted = $_POST['l_password'];
+
+  $query = "SELECT id, encrypted_password FROM Tusuario WHERE email = '".$email_posted."'";
+
+  $result = mysqli_query($db, $query) or die('Query error');
+
+
+  if (mysqli_num_rows($result) > 0) {
+
+      $only_row = mysqli_fetch_array($result);
+
+      if (password_verify($password_posted, $only_row['encrypted_password'])) {
+        //Hasta aqui recupera la tuplas donde el email guardado sea igual al email qu nosotros recuperamos de login.php
+        //si esa tupla existe y las contraseñas son iguales inicio una sesión
+          session_start();
+          $_SESSION['user_id'] = $only_row[0];
+          header("Location: main.php");
+
+      } else {
+
+          header('Location: login.php?login_failed_password=True');
+          echo 'La contraseña es incorrecta';
+
+      }
+}

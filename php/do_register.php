1bbdfec93f9fe722b1c8f5bcda2dac7ffe742697
Luxary147 <95914993+Luxary147@users.noreply.github.com>
2022-04-29 12:25:10 +0200

    Añadido otra linea de pruebas

--- php/do_register.php
+++ php/do_register.php
@@ -27,6 +27,8 @@ $passH = password_hash($_POST['pass'], PASSWORD_BCRYPT);
 echo ($passH);
 $stmt -> bind_param("sss", $nick, $email, $pass);
 
+echo ($stmt);
+
 if ($stmt == True) {
   echo ("Funciona");
 }else{

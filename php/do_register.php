2b06eec313c45c5b2a6c287dafde251278824f4a
Luxary147 <95914993+Luxary147@users.noreply.github.com>
2022-05-03 11:53:07 +0200

    Cambiadas comillas dobles a simples en sss

--- php/do_register.php
+++ php/do_register.php
@@ -31,9 +31,8 @@ $sql = "INSERT INTO Tusuario (nick, email, encrypted_password) VALUES  (?, ?, ?)
 $stmt = $db -> prepare($sql);
 $passH = password_hash($_POST['pass'], PASSWORD_BCRYPT);
 echo ($passH);
-$stmt -> bind_param("sss", $nick, $email, $pass);
+$stmt -> bind_param('sss', $nick, $email, $pass);
 
-echo ($stmt);
 echo ("hola");
 
 if ($stmt == True) {

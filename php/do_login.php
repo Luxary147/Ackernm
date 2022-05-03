8edfca82c4732e93e14c74797a41f89d0308e9cd
Luxary147 <95914993+Luxary147@users.noreply.github.com>
2022-05-03 12:31:26 +0200

    Limpieza de código de pruebas y prueba final
    He eliminado los echos que tenia de prueba , así como cambiado el bucle if 
    donde compara ambas contraseñas, la de la base de datos , con la que nosotros hemos introducido , para saber si el fallo era ahi
    
    De ser el fallo hay, sería cosa de que al harcodear en la base de datos la contraseña normal , sin estar hasheada.
    Luego al compararlas serian distintas.

--- php/do_login.php
+++ php/do_login.php
@@ -4,34 +4,14 @@
   $email_posted = $_POST['l_email'];
   $password_posted = $_POST['l_password'];
 
-echo ($email_posted);
-echo ($password_posted);
-
   $query = "SELECT id, contraseña FROM Tusuario WHERE email = '".$email_posted."'";
-
-echo ($query);
-
   $result = mysqli_query($db, $query) or die('Query error');
 
-echo ("hola");
-
   if (mysqli_num_rows($result) > 0) {
+     $only_row = mysqli_fetch_array($result);
     
-    echo ("adios");
-
-      $only_row = mysqli_fetch_array($result);
-    
-    echo ("a");
-
-    echo ($only_row[0]);
-    echo ($only_row[1]);
-    
-    echo (password_verify($password_posted, $only_row[1]));
-    
-    
-      if (password_verify($password_posted, $only_row[1])) {
-        //Hasta aqui recupera la tuplas donde el email guardado sea igual al email qu nosotros recuperamos de login.php
-        //si esa tupla existe y las contraseñas son iguales inicio una sesión
+      //if (password_verify($password_posted, $only_row[1])) {
+    if (password_posted = $only_row[1] )
           session_start();
           $_SESSION['user_id'] = $only_row[0];
           header("Location: main.php");

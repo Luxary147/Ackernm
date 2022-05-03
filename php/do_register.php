a018fe3bf5cb8f5cb239edef2db2d67a8ee8c166
Luxary147 <95914993+Luxary147@users.noreply.github.com>
2022-05-03 12:37:52 +0200

    Correción de código

--- php/do_register.php
+++ php/do_register.php
@@ -2,7 +2,6 @@
 
 //ini_set('display_errors', 'On');
 //require __DIR__ . '/../php/db_connection.php';
-
 //$mysqli = get_db_connection_or_die();
 
 $db = mysqli_connect('localhost', 'root', '1234', 'Ackernm') or die('Fail');
@@ -12,39 +11,22 @@ if (!$db) {
 }
 echo "Connected successfully";
 
-
 $nick = $_POST['nick'];
 $email = $_POST['email'];
 $pass = $_POST['pass'];
 $pass2 = $_POST['pass2'];
 
-echo ($nick);
-echo ($email);
-echo ($pass);
-
-
   if($pass != $pass2){
     die("Las contraseñas no coinciden");
   }
 
-$sql = "INSERT INTO Tusuario (nick, email, encrypted_password) VALUES  (?, ?, ?)";
+$sql = "INSERT INTO Tusuario (nick, email, contraseña) VALUES  (?, ?, ?)";
 $stmt = $db -> prepare($sql);
 $passH = password_hash($_POST['pass'], PASSWORD_BCRYPT);
-echo ($passH);
 $stmt -> bind_param('sss', $nick, $email, $passH);
-
-echo ("hola");
-
-if ($stmt == True) {
-  echo ("Funciona");
-}else{
-  echo (" casca");
-}
-
 $stmt -> execute();
 $stmt -> close();
   
-
 
 header("Location: register.php")
 ?>

b50cc8e5c960c8155b8f71a57f3a205b72f54553
Luxary147 <95914993+Luxary147@users.noreply.github.com>
2022-05-05 13:45:32 +0200

    Añadido un html dentro de php
    Espera el tiempo antes de hacer la redirección , pero no mostraba el mensaje , ya que se quedaba esperando en la pagina de resgister.php y no en esta

--- php/UsuarioCreado.php
+++ php/UsuarioCreado.php
@@ -1,10 +1,11 @@
 <?php
-
+  echo "<!DOCTYPE html>";
+  echo "<html>";
+  echo "<head><meta charset='UTF8'></head>";
+  echo "<body>";
   echo ('<p id="mensaje" > Usuario creado Con Exito </p>');
-
+  echo "</body>";
+  echo "</html>";
   sleep(10);
-
-header("Location: login.php")
-  
-
+  header("Location: login.php")
 ?>

c43ea0cc45076bcc0ba8433efc29e2fb4e3a5926
Luxary147 <95914993+Luxary147@users.noreply.github.com>
2022-05-06 13:08:27 +0200

    Modificado el código para que muestre mensajes en caso de campos invalidos
    He ajustado el código para que compruebe , en este caso si la contraseña introducida tiene mas de 6 caracteres o no.
    
    Ademas he desplazado un bloque de código para que solo ejecute el insert y el redirect en caso de que todo este correcto. (en teoria, me falta probarlo)
    
    Asi como he añadido un enlace para volver a register.php

--- php/do_register.php
+++ php/do_register.php
@@ -4,30 +4,34 @@ $db = mysqli_connect('localhost', 'root', '1234', 'Ackernm') or die('Fail');
 if (!$db) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+//echo "Connected successfully";
 
 $nick = $_POST['nick'];
 $email = $_POST['email'];
 $pass = $_POST['pass'];
 $pass2 = $_POST['pass2'];
 
-  if($pass != $pass2){
-    die("Las contraseñas no coinciden");
-  }
-
-$sql = "INSERT INTO Tusuario (nombre, contraseña, email) VALUES  (?, ?, ?)";
-$stmt = $db -> prepare($sql);
-$passH = password_hash($_POST['pass'], PASSWORD_BCRYPT);
-$stmt -> bind_param('sss', $nick, $passH, $email);
-
-echo ("bind_param");
-$stmt -> execute();
-echo ("ejecucion");
-$stmt -> close();
-  
-
-header("Location: login.php");
-echo ('<p id="mensaje" > Usuario creado Con Exito </p>');
+if ((strlen($pass) > 6 ){
+    
+      if($pass != $pass2){
+        die("Las contraseñas no coinciden");
+          
+      }else{
+            $sql = "INSERT INTO Tusuario (nombre, contraseña, email) VALUES  (?, ?, ?)";
+            $stmt = $db -> prepare($sql);
+            $passH = password_hash($_POST['pass'], PASSWORD_BCRYPT);
+            $stmt -> bind_param('sss', $nick, $passH, $email);
+            $stmt -> execute();
+            $stmt -> close();
+            header("Location: login.php");
+      }
+  }else{
+    echo ("La contraseña es demasiado corta");
+ }
+//echo ('<p id="mensaje" > Usuario creado Con Exito </p>');
+    
 ?>
+
+<a href="register.php"> Volver al registro</a>
 
 

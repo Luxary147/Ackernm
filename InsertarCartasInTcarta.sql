de1b9d06afb9e5ffb3115d27bc6e361f5d4cfe61
Luxary147 <95914993+Luxary147@users.noreply.github.com>
2022-05-20 20:57:28 +0200

    Añadidas comillas simples

--- InsertarCartasInTcarta.sql
+++ InsertarCartasInTcarta.sql
@@ -1,20 +1,20 @@
-INSERT INTO Tcarta (nombre , url_imagen) VALUES ( Mage , https://raw.githubusercontent.com/Luxary147/Ackernm/main/raw/Mago_TradingCard(common).png );
+INSERT INTO Tcarta (nombre , url_imagen) VALUES ( 'Mage' , 'https://raw.githubusercontent.com/Luxary147/Ackernm/main/raw/Mago_TradingCard(common).png ');
 
-INSERT INTO Tcarta (nombre , url_imagen) VALUES ( Hunter , https://raw.githubusercontent.com/Luxary147/Ackernm/main/raw/Hunter_TradingCard%20(common).png);
+INSERT INTO Tcarta (nombre , url_imagen) VALUES ('Hunter' , 'https://raw.githubusercontent.com/Luxary147/Ackernm/main/raw/Hunter_TradingCard%20(common).png');
 
-INSERT INTO Tcarta (nombre , url_imagen) VALUES ( Demon Hunter , https://raw.githubusercontent.com/Luxary147/Ackernm/main/raw/Demon_Hunter_TradingCard%20(epic).png);
+INSERT INTO Tcarta (nombre , url_imagen) VALUES ( 'Demon Hunter' , 'https://raw.githubusercontent.com/Luxary147/Ackernm/main/raw/Demon_Hunter_TradingCard%20(epic).png');
 
-INSERT INTO Tcarta (nombre , url_imagen) VALUES ( Priest , https://raw.githubusercontent.com/Luxary147/Ackernm/main/raw/Priest_TradingCard(rare).png);
+INSERT INTO Tcarta (nombre , url_imagen) VALUES ( 'Priest', 'https://raw.githubusercontent.com/Luxary147/Ackernm/main/raw/Priest_TradingCard(rare).png');
 
-INSERT INTO Tcarta (nombre , url_imagen) VALUES ( Warrior , https://raw.githubusercontent.com/Luxary147/Ackernm/main/raw/Warrior_TradingCard(rare).png);
+INSERT INTO Tcarta (nombre , url_imagen) VALUES ( 'Warrior' , 'https://raw.githubusercontent.com/Luxary147/Ackernm/main/raw/Warrior_TradingCard(rare).png');
 
-INSERT INTO Tcarta (nombre , url_imagen) VALUES ( Rogue , https://raw.githubusercontent.com/Luxary147/Ackernm/main/raw/Rogue_TradingCard%20(rare).png);
+INSERT INTO Tcarta (nombre , url_imagen) VALUES ( 'Rogue' , 'https://raw.githubusercontent.com/Luxary147/Ackernm/main/raw/Rogue_TradingCard%20(rare).png');
 
-INSERT INTO Tcarta (nombre , url_imagen) VALUES ( Druid , https://raw.githubusercontent.com/Luxary147/Ackernm/main/raw/Druid_TradingCard%20(common).png);
+INSERT INTO Tcarta (nombre , url_imagen) VALUES ( 'Druid' , 'https://raw.githubusercontent.com/Luxary147/Ackernm/main/raw/Druid_TradingCard%20(common).png');
 
-INSERT INTO Tcarta (nombre , url_imagen) VALUES ( Shaman , https://raw.githubusercontent.com/Luxary147/Ackernm/main/raw/Shaman_TradingCard(epic).png);
+INSERT INTO Tcarta (nombre , url_imagen) VALUES ( 'Shaman' , 'https://raw.githubusercontent.com/Luxary147/Ackernm/main/raw/Shaman_TradingCard(epic).png');
 
-INSERT INTO Tcarta (nombre , url_imagen) VALUES ( Warlock , https://raw.githubusercontent.com/Luxary147/Ackernm/main/raw/Warlock_TradingCard(epic).png);
+INSERT INTO Tcarta (nombre , url_imagen) VALUES ( 'Warlock' , 'https://raw.githubusercontent.com/Luxary147/Ackernm/main/raw/Warlock_TradingCard(epic).png');
 
-INSERT INTO Tcarta (nombre , url_imagen) VALUES ( Paladin , https://raw.githubusercontent.com/Luxary147/Ackernm/main/raw/Paladin_TradingCard(rare).png);
+INSERT INTO Tcarta (nombre , url_imagen) VALUES ( 'Paladin' , 'https://raw.githubusercontent.com/Luxary147/Ackernm/main/raw/Paladin_TradingCard(rare).png');
 

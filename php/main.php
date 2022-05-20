c6f5ef6b0213fc253ccfdebe2c05ccc7f40dd2a4
Luxary147 <95914993+Luxary147@users.noreply.github.com>
2022-05-20 12:24:46 +0200

    Moficación y reorganización de main.php
    (esto aun esta sin probar)
    
    He añadido la segunda parte del código para recuperar y comparar , los ids de las cartas que ya tiene el usuario en su colección .
    
    Para esto he reorganizado el código para que esta parte solo se ejecute si el usuario esta logeado , (tiene sentido verdad),
    por eso he puesto primero la parte donde recupero y muestro tosa la colección y luego es cunado realizo la validación de inicio de sesión .

--- php/main.php
+++ php/main.php
@@ -10,70 +10,93 @@
     </head>
     
     <body> 
-    <?php
         
-    session_start();
-        
-    if (empty($_SESSION['user_id'])) { 
-        //bloque php donde compruebo si el ususario esta logeado o no 
-    ?>
-        <div class="contenedorLogin">
-            <p>No estás logueado, Inicia Sesión <a href='/login.php'> aquí</a>.</p>
-        </div>
+         <?php
 
-    <?php
-    } else {
-    ?>
-        <div class="main">
-            <p > Estas logueado buen trabajo </p>
+         $db = mysqli_connect('localhost', 'root', '1234', 'Ackernm') or die('Fail');
 
-        </div>
-        
-     <?php
-           }
-     ?>
-        
-     <?php
-     
-     $db = mysqli_connect('localhost', 'root', '1234', 'Ackernm') or die('Fail');
+          if (!$db) {
+              die("Connection failed: " . mysqli_connect_error());
+          }
 
-      if (!$db) {
-          die("Connection failed: " . mysqli_connect_error());
-      }
-      
 
-      $query = "SELECT id, nombre, url_imagen, essencias FROM Tcarta" ;
-      $cartas = mysqli_query($db, $query) or die('Query error');
-      
-      /*compruebo si existen cartas*/
-      if (mysqli_num_rows($cartas) > 0) {     
-      ?>
-      <div>
-      <?php
-         /*revisar esta linea*/
-         while ($only_row = mysqli_fetch_array($cartas)){
-            echo '<div id="carta">
-                        <img class ="imagen" src="'.$only_row[2].'" alt="'.$only_row[1].'">
-                  </div>
-                  <div id="formu_'.$only_row[0].'">
-                      <form action="do_buy_card.php" method="post" id="do_buy_card">
-                          <h2>'.$only_row[1].'</h2>
-                          <p>'.$only_row[3].'</p>
-                          <input id="'.$only_row[0].'" name="cartaid" type="hidden">
-                          <input type="submit" value="Comprar">
-                      </form>
-                  </div>';
-            }
+          $query = "SELECT id, nombre, url_imagen, essencias FROM Tcarta" ;
+          $cartas = mysqli_query($db, $query) or die('Query error');
+
+          /*compruebo si existen cartas*/
+          if (mysqli_num_rows($cartas) > 0) {     
           ?>
-         </div>
-         
-      <?php
-      }else{
-        echo ("Error al recuperar los datos");
-      /*ya que siempre exestiran cartas en la base de datos , por que asi esta definida la coleccion*/
-      }
-      
-      mysqli_close($db);
-      ?> 
+        
+          <div id="coleccion">
+              
+          <?php
+             /*revisar esta linea*/
+             while ($only_row = mysqli_fetch_array($cartas)){
+                echo '<div id="carta'.$only_row[0].'">
+                            <img class ="imagen" src="'.$only_row[2].'" alt="'.$only_row[1].'">
+                      </div>
+                      <div id="formu_'.$only_row[0].'">
+                          <form action="do_buy_card.php" method="post" id="do_buy_card">
+                              <h2>'.$only_row[1].'</h2>
+                              <p>'.$only_row[3].'</p>
+                              <input id="'.$only_row[0].'" name="cartaid" type="hidden">
+                              <input type="submit" value="Comprar">
+                          </form>
+                      </div>';
+                }
+              ?>
+             </div>
+
+          <?php
+              }else{
+                echo ("Error al recuperar los datos");
+              /*ya que siempre exestiran cartas en la base de datos , por que asi esta definida la coleccion*/
+              }
+           ?>
+        
+        
+        <?php
+        
+        session_start();
+
+        if (empty($_SESSION['user_id'])) { 
+            //bloque php donde compruebo si el ususario esta logeado o no 
+        ?>
+            <div class="contenedorLogin">
+                <p>No estás logueado, Inicia Sesión <a href='/login.php'> aquí</a>.</p>
+            </div>
+
+        <?php
+        } else {
+       //Hasta aqui seria la parte de recuperar la coleccion completa.
+      //Ahora vendria la parte de recuperar los datos de la tabla Tcartacoleccion , para saber que cartas ya tiene desbloqueada ese usuario
+        ?>
+            <div class="main">
+                <p > Estas logueado buen trabajo </p>
+            </div>
+         <?php
+            
+          $query2 = "SELECT idCarta FROM TCartaUsuario WHERE idUsuario = '".$_SESSION['user_id']"'";
+          $earned = mysqli_query($db, $query2) or die('Query error');
+
+              if (mysqli_num_rows($earned) > 0) {
+                 $Cearn = mysqli_fetch_array($earned);
+
+                while ($Cearn = mysqli_fetch_array($earned)){
+
+                    for ($only_row = mysqli_fetch_array($cartas){
+
+                        if ($only_row[0] == $Cearn[0]) {
+
+                        $obtenidas_push($only_row[0]);
+
+                        }
+                     }
+                 }
+             }
+         }
+         mysqli_close($db);        
+         ?>
+        
     </body>
 </html>

6f2fe9f3662e367ba6176bb974bbe01475a93032
Luxary147 <95914993+Luxary147@users.noreply.github.com>
2022-05-26 21:43:02 +0200

    Añadidas cabeceras html

--- php/C_detail.php
+++ php/C_detail.php
@@ -1,4 +1,15 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="C_detail.css">
+        <title>Document</title>
+    </head>
+    
+    <body> 
+    <?php
         session_start();
             
         if (isset($_SESSION['user_id'])) { 
@@ -42,3 +53,5 @@
         }
 
 ?>
+    </body>
+</html>

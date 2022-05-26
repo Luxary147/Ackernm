ee8eb975d8dc7c5ad50907e7e86cdb1e7845fda8
Luxary147 <95914993+Luxary147@users.noreply.github.com>
2022-05-26 20:17:06 +0200

    Se ha creado una pagina de Detalle de una carta
    He creado una pagina donde nos muestra con detalle la carta , la cual vamos a desbloquear;
    asi como al lado de esta los datos de dicha carta y el boton de compra.

---
+++ php/C_detail.php
@@ -0,0 +1,37 @@
+<?php
+        session_start();
+            
+        if (isset($_SESSION['user_id'])) { 
+
+        //recupero la información del input del formulario 
+        $idCarta = $_POST['cartaid'];
+        $nombreCarta = $_POST['cartanombre'];
+        $urlimagen = $_POST['cartaurl'];
+        $costeEssencias = $_POST['cartaessencias'];
+
+        //almaceno el id del usuario logeado
+        $idUsuario = $_SESSION['user_id'];
+
+          
+        echo '<div id="carta">
+                    <img class ="imagenalfa" src="'.$urlimagen.'" alt="'.$nombreCarta.'">
+              </div>
+
+              <div id="formu">
+                    <form action="do_buy_card.php" method="post" id="do_buy_card">
+                        <h2>'.$nombreCarta.'</h2>
+                        <p>'.$costeEssencias.'</p>
+                        <input id="cartaid" name="cartaid" type="hidden" value="'.$idCarta.'">
+                        <input type="submit" value="Comprar">
+                    </form>
+               </div>';
+            
+        }else{
+            
+            echo '<div class="contenedorLogin">
+                      <p>No estás logueado, Inicia Sesión <a href='/login.php'> aquí</a>.</p>
+                  </div>';
+                
+        }
+
+?>

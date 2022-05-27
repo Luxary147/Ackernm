42e6801fc704bd158904f90b057cb05a09bbf4cf
Luxary147 <95914993+Luxary147@users.noreply.github.com>
2022-05-27 19:22:06 +0200

    Update do_buy_card.php

--- php/do_buy_card.php
+++ php/do_buy_card.php
@@ -53,7 +53,7 @@
           }
                 
                  echo '<h2> Compra realizada con exito </h2>';
-                 header("Refresh: 10; main.php");
+                // header("Refresh: 10; main.php");
 
         } else {
             echo '<h2> El usuario tiene que estar logeado para poder adquerir cartas a su coleción </h2>

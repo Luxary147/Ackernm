4d2b6ab9ec7c2b14c14238fd048c2357953df97e
Luxary147 <95914993+Luxary147@users.noreply.github.com>
2022-05-27 22:31:15 +0200

    Update do_register.php

--- php/do_register.php
+++ php/do_register.php
@@ -1,40 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body{
+            background-color: rgb(106, 105, 105);
+            box-sizing: border-box;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        p{ 
+            color:white;
+            font-size:22px;
+            margin-top:4%;
+            text-align:center;
+        }
+    </style>
+    <title>Document</title>
+</head>
+
 <?php
-$db = mysqli_connect('localhost', 'root', '1234', 'Ackernm') or die('Fail');
+        $db = mysqli_connect('localhost', 'root', '1234', 'Ackernm') or die('Fail');
 
-if (!$db) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-//echo "Connected successfully";
+        if (!$db) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        //echo "Connected successfully";
 
-$nick = $_POST['nick'];
-$email = $_POST['email'];
-$pass = $_POST['pass'];
-$pass2 = $_POST['pass2'];
+        $nick = $_POST['nick'];
+        $email = $_POST['email'];
+        $pass = $_POST['pass'];
+        $pass2 = $_POST['pass2'];
 
-if (strlen($pass) > 6 ){
-    
-      if($pass != $pass2){
-        die("Las contraseñas no coinciden");
-          
-      }else{
-            $sql = "INSERT INTO Tusuario (nombre, contraseña, email) VALUES  (?, ?, ?)";
-            $stmt = $db -> prepare($sql);
-            $passH = password_hash($_POST['pass'], PASSWORD_BCRYPT);
-            $stmt -> bind_param('sss', $nick, $passH, $email);
-            $stmt -> execute();
-            $stmt -> close();
-            //header("Location: login.php");
-            header("Refresh: 15; login.php");
- 
-            echo 'Esto ha esperado';
-      }
-  }else{
-    echo ("La contraseña es demasiado corta");
- }
-//echo ('<p id="mensaje" > Usuario creado Con Exito </p>');
+        if (strlen($pass) > 6 ){
+
+              if($pass != $pass2){
+                die '<p> Las contraseñas no coinciden </p>';
+                  header("Refresh: 2; register.php");
+
+              }else{
+                    $sql = "INSERT INTO Tusuario (nombre, contraseña, email) VALUES  (?, ?, ?)";
+                    $stmt = $db -> prepare($sql);
+                    $passH = password_hash($_POST['pass'], PASSWORD_BCRYPT);
+                    $stmt -> bind_param('sss', $nick, $passH, $email);
+                    $stmt -> execute();
+                    $stmt -> close();
+                    header("Refresh: 3; login.php");
+                    echo '<p> Usuario registrado con Exito </p>';
+              }
+          }else{
+            echo '<p> La contraseña es demasiado corta </p>';
+            header("Refresh: 2; register.php");
+         }
     
 ?>
+</html>
 
-<a href="register.php"> Volver al registro</a>
+
 
 

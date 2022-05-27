994a138d65b19339614e9a5e0c3c12d53d8c2e0d
Luxary147 <95914993+Luxary147@users.noreply.github.com>
2022-05-27 21:15:31 +0200

    Añadidas cabecera html y estilos basicos

--- php/do_buy_card.php
+++ php/do_buy_card.php
@@ -1,4 +1,25 @@
-<?php
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h2 {
+            margin-top:12%;
+            text-align: center;
+            justify-content: center;
+        }
+        p{
+            margin-top:12%;
+            text-align: center;
+            justify-content: center;
+        }
+    </style>
+</head>
+        <?php
         session_start();
             
         if (isset($_SESSION['user_id'])) { 
@@ -63,3 +84,4 @@
         
         mysqli_close($db);
 ?>
+</html>

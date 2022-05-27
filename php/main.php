d38eeddb30886079bb41f18bc17129215e71191a
Luxary147 <95914993+Luxary147@users.noreply.github.com>
2022-05-27 11:55:17 +0200

    Reorganizacion total del código main.php
    Se a creado una forma de visualización donde puedes ver toda la coleccion , sin poder interactuar con hecha , disponible sin iniciar sesion
    
    Y el otro metodo , nos mostrara las cartas desbloqueadas de la coleccion y las que no

--- php/main.php
+++ php/main.php
@@ -19,12 +19,47 @@
          <?php
         session_start();
         
-         $db = mysqli_connect('localhost', 'root', '1234', 'Ackernm') or die('Fail');
+        $db = mysqli_connect('localhost', 'root', '1234', 'Ackernm') or die('Fail');
           if (!$db) {
               die("Connection failed: " . mysqli_connect_error());
           }
         
+        if (empty($_SESSION['user_id'])) { 
+            //bloque php donde compruebo si el ususario esta logeado o no 
+        ?>
+            <div class="contenedorLogin">
+                <p>No estás logueado, para poder interactuar con la colección, Inicia Sesión <a href='/login.php'> aquí</a>.</p>
+            </div>
         
+          $query = "SELECT id, nombre, url_imagen, essencias FROM Tcarta" ;
+          $cartas = mysqli_query($db, $query) or die('Query error');
+        
+          if (mysqli_num_rows($cartas) > 0) {     
+          ?>
+        
+          <div id="coleccion">
+              
+          <?php
+             while ($only_row = mysqli_fetch_array($cartas)){
+                     
+                     echo '<div id="carta'.$only_row[0].'">
+                            <img class ="imagen" src="'.$only_row[2].'" alt="'.$only_row[1].'">
+                            </div>'
+              ?>
+             </div>
+          <?php
+              }else{
+                echo ("Error al recuperar los datos");
+              /*ya que siempre exestiran cartas en la base de datos , por que asi esta definida la coleccion*/
+              }
+            
+        } else {
+        ?>
+            <div class="main">
+                <p > Estas logueado buen trabajo </p>
+            </div>
+        
+        <?php
           $query = "SELECT Tcarta.id, nombre, url_imagen, essencias , idUsuario FROM Tcarta LEFT JOIN TcartaUsuario ON Tcarta.id = TcartaUsuario.idCarta" ;
           $cartas = mysqli_query($db, $query) or die('Query error');
         
@@ -34,7 +69,6 @@
           <div id="coleccion">
               
           <?php
-              $Tcartas = [];
              while ($only_row = mysqli_fetch_array($cartas)){
                  array_push($Tcartas, $only_row[0], $only_row[1], $only_row[2], $only_row[3],$only_row[4]);
                  
@@ -45,8 +79,6 @@
                             </div>';
                      
                  }else{
-                     //He convertido las carta las carta que no tenemos en nuestra colección en botones , con los que puedes interactuar desde php
-                     //A mayores he convertido el formulario , para que me envie los datos a la pagina de Detalle de la carta , sin que este muestre nada en main.php
                   
                     echo '<div id="formu">
                                <form action="C_detail.php" method="post" id="C_detail">
@@ -66,35 +98,10 @@
                 }
               ?>
              </div>
-          <?php
-              }else{
-                echo ("Error al recuperar los datos");
-              /*ya que siempre exestiran cartas en la base de datos , por que asi esta definida la coleccion*/
-              }
-           ?>
-        
-        
-        <?php
-        
-        if (empty($_SESSION['user_id'])) { 
-            //bloque php donde compruebo si el ususario esta logeado o no 
-        ?>
-            <div class="contenedorLogin">
-                <p>No estás logueado, Inicia Sesión <a href='/login.php'> aquí</a>.</p>
-            </div>
-        <?php
-        } else {
-       //Hasta aqui seria la parte de recuperar la coleccion completa.
-      //Ahora vendria la parte de recuperar los datos de la tabla Tcartacoleccion , para saber que cartas ya tiene desbloqueada ese usuario
-        ?>
-            <div class="main">
-                <p > Estas logueado buen trabajo </p>
-            </div>
          <?php
             
          }
          mysqli_close($db);   
-         print_r($cartas);
          ?>
         
     </body>

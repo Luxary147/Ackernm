4fc4d9eb29b064b1493a83b90e4bae041d9a275b
Luxary147 <95914993+Luxary147@users.noreply.github.com>
2022-05-28 20:31:20 +0200

    codigo antiguo

--- php/main.php
+++ php/main.php
@@ -9,63 +9,42 @@
     </head>
     
     <body> 
-        <?php
+        <nav class="selecciones">
+            <ul>
+                <a href="?main=colecion"><li> Colección </li></a>
+                <a href="?main=dado"><li> Tirada </li></a>
+            </ul>
+        </nav>
+        <section>
+         <?php
+            
+            if (isset($_GET["main"])){
+                switch ($_GET["main"]){
+                    case 'coleccion':
+                        include("coleccion.php");
+                        break;
+
+                    case 'dado':
+                        include ("tirada.php");
+                        break;
+                }
+            }
         
         session_start();
         
-            if (empty($_SESSION['user_id'])) { 
+        $db = mysqli_connect('localhost', 'root', '1234', 'Ackernm') or die('Fail');
+          if (!$db) {
+              die("Connection failed: " . mysqli_connect_error());
+          }
+        
+        if (empty($_SESSION['user_id'])) { 
+            //bloque php donde compruebo si el ususario esta logeado o no 
         ?>
             <div class="contenedorLogin">
                 <p>No estás logueado, para poder interactuar con la colección, Inicia Sesión <a href='/login.php'> aquí</a>.</p>
             </div>
         
         <?php
-                
-          } else {
-                
-        ?>
-            <div class="main">
-                <p > Estas logueado buen trabajo </p>
-            </div>
-        <?php
-            }
-        ?>
-         
-        <nav class="selecciones">
-            <ul>
-                <a href="?main=coleccion"><li> Colección </li></a>
-                <a href="?main=dado"><li> Tirada </li></a>
-            </ul>
-        </nav>
-
-        <section>
-            
-         <?php
-            if (isset($_GET["main"])){
-                switch ($_GET["main"]){
-                    case 'coleccion':
-                        include("colecion.php");
-                        break;
-                       
-                    case 'dado':
-                        include ("tirada.php");
-                        break;
-                }
-            }
-         
-            ?>
-        </section>
-        
-        <?php
-          $db = mysqli_connect('localhost', 'root', '1234', 'Ackernm') or die('Fail');
-          if (!$db) {
-              die("Connection failed: " . mysqli_connect_error());
-          }
-        
-        if (empty($_SESSION['user_id'])) { 
-          
-          $idUsuario = $_SESSION['user_id'];
-          
           $query = "SELECT id, nombre, url_imagen, essencias FROM Tcarta" ;
           $cartas = mysqli_query($db, $query) or die('Query error');
         
@@ -76,22 +55,25 @@
               
           <?php
              while ($only_row = mysqli_fetch_array($cartas)){
-
                      echo '<div class="imagenalfa">
                             <img class ="imagen" src="'.$only_row[2].'" alt="'.$only_row[1].'">
                             </div>';
                  }
               ?>
              </div>
-
           <?php
-            
               }else{
                 echo ("Error al recuperar los datos");
+              /*ya que siempre exestiran cartas en la base de datos , por que asi esta definida la coleccion*/
               }
             
         } else {
-
+        ?>
+            <div class="main">
+                <p > Estas logueado buen trabajo </p>
+            </div>
+        
+        <?php
           $query = "SELECT Tcarta.id, nombre, url_imagen, essencias , idUsuario FROM Tcarta LEFT JOIN TcartaUsuario ON Tcarta.id = TcartaUsuario.idCarta" ;
           $cartas = mysqli_query($db, $query) or die('Query error');
         
@@ -101,20 +83,15 @@
           <div id="coleccion">
               
           <?php
-            $Tcartas= [];
              while ($only_row = mysqli_fetch_array($cartas)){
                  array_push($Tcartas, $only_row[0], $only_row[1], $only_row[2], $only_row[3],$only_row[4]);
                  
                  if ($only_row[4] != NULL){
-                   
-                   if ($only_row[4] == $idUsuario){
                      
                      echo '<div id="carta'.$only_row[0].'">
                             <img class ="imagen" src="'.$only_row[2].'" alt="'.$only_row[1].'">
                             </div>';
-                   }else{
-                     $cartarepetida = $cartarepetida + 1;
-                   }
+                     
                  }else{
                   
                     echo '<div id="formu">
@@ -138,8 +115,8 @@
             
          }
         }
-         print_r($Tcartas);
          mysqli_close($db);   
          ?>
+        
     </body>
 </html>

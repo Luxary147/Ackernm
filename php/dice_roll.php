44bd1b3f4e5259b71166c4fb9302d4ef295ff00a
Luxary147 <95914993+Luxary147@users.noreply.github.com>
2022-05-29 20:16:13 +0200

    Update dice_roll.php

--- php/dice_roll.php
+++ php/dice_roll.php
@@ -5,6 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+        <Style>
+
+    *{
+        background-color: rgb(106, 105, 105);
+        color:rgba(222, 226, 240, 0.966);
+    }
+        h1{
+
+            margin-top:12%;
+            text-align: center;
+        }
+
+        h3{
+            margin-top:6%;
+            text-align: center;
+        }
+
+    </Style>
 </head>
 <body>
   <?php
@@ -18,11 +36,11 @@
           $ganancias = $_SESSION['ganancias'];
           $total = $_SESSION['total'];
                      
-            echo "<h1> Los Dioses han hablado , has sacado un $numero </h1>";
+            echo "<h1> Los Dioses han hablado , has sacado un <bolder>$numero</bolder> </h1>";
 
             echo "<h3> Tras haber apostado $essencias essencias, has conseguido ganar $ganancias essencias más </h3>";
             
-            echo "<h3> Ahora tienes $total essencias  </h3>";
+            echo "<h3> Ahora tienes <bolder> $total</bolder> essencias  </h3>";
 
          }
     
